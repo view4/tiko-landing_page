a54f5577d97c76cac3c25ba03b53612ca3a58e5b
Gabriel Morris <morrisgaby987@googlemail.com>
2019-11-14 18:52:42 +0200

    header for a landing page

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('tiko page')
+/*
+The welcome section. 
+
+Why is it important: 
+- care for others 
+- to bring feeling to the page 
+- not to be too content heavy. 
+- perhaps linked with some kind of sanctification
+- speak of what is important.
+
+So I think I want a background image with any content before this(reminder between your eyes). And I think with the background image, I do not want it to be something which is too much. So what do I do with the image what kind of image is it going to be . perhaps something whith like an earth in hands kind of image and then we can have some kind of content before it.
+
+So what content should there be before it? Something light and linked with the essence of Tiko, something linked with heal the world but less cringey perhaps. Something to do with the potnetial we have in the palm of our hands.
+
+*/
